d25cc8bfb8ee7a6cc59135745c40e342a8b561e4
raghavshuklaofficial <108609058+raghavshuklaofficial@users.noreply.github.com>
2025-05-26 10:16:23 +0530

    Basic Utilities Required
    Contains all the basic utilities being required in all components of the compiler

---
+++ Compiler/interface.h
@@ -0,0 +1,12 @@
+// Contains all the basic utilities being required in all components of the compiler
+#ifndef INTF_
+#define INTF_
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif
