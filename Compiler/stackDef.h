5aee1f59d9377ab688aba01733243f12b566663e
raghavshuklaofficial <108609058+raghavshuklaofficial@users.noreply.github.com>
2025-05-26 10:11:27 +0530

    manages stack operations for n-ary traversal and parsing

---
+++ Compiler/stackDef.h
@@ -0,0 +1,15 @@
+#ifndef STACK_
+#define STACK_
+#include "nary_treeDef.h"
+
+typedef struct StackNode {
+    NaryTreeNode* TREE_NODE;
+    struct StackNode* next;
+} StackNode;
+
+typedef struct Stack {
+    StackNode* HEAD;
+    int NUM_NODES;
+} Stack;
+
+#endif
